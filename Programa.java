2e450e692bda2eac7f0c2e6cf3a6119a86c7b3ce
Luciano da Rocha Schwalm <lucischwalm@MacBook-Air-de-Luciano-2.local>
2023-09-15 19:57:46 -0300

    criacao de switch case e correcao dos valores do hashmap

--- Programa.java
+++ Programa.java
@@ -5,11 +5,13 @@ import java.util.HashMap;
 
 public class Programa {
     private String path;
+    private int pc;
     private HashMap<Integer, String> instrucoes;
     private HashMap<Integer, String> dados;
 
     public Programa(String path) {
         this.path = path;
+        this.pc = 0;
         instrucoes = new HashMap<Integer, String>();
         dados = new HashMap<Integer, String>();
         lePrograma(path);
@@ -19,20 +21,22 @@ public class Programa {
         File file = new File(path);
         FileReader fr;
         int chave = 0;
+
         try {
             fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
             String line = "";
+
             while (!line.contains(".enddata")) {
                 line = br.readLine();
                 if (line.contains(".code")) {
-                    line = br.readLine().replaceAll("\u00A0", "").trim();
+                    line = br.readLine().trim();
                     while (!line.contains(".endcode")) {
                         // cria hashmap para ler todas as instrucoes
                         instrucoes.put(chave++, line);
 
                         // atualiza a proxima linha
-                        line = br.readLine();
+                        line = br.readLine().trim();
                     }
                 }
 
@@ -52,8 +56,54 @@ public class Programa {
             System.out.println(instrucoes);
             System.out.println(dados);
             br.close();
+
+            verificaInstrucoes();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public void verificaInstrucoes() {
+        instrucoes.values().forEach(i -> rodaInstrucao(i));
+    }
+
+    public void rodaInstrucao(String value) {
+        switch (value.split(" ")[0]) {
+            case "add":
+                System.out.println("add");
+                break;
+            case "sub":
+                System.out.println("sub");
+                break;
+            case "mult":
+
+                break;
+            case "div":
+
+                break;
+            case "load":
+                System.out.println("load");
+                break;
+            case "store":
+                System.out.println("store");
+                break;
+            case "brany":
+
+                break;
+            case "brpos":
+
+                break;
+            case "brzero":
+
+                break;
+            case "brneg":
+
+                break;
+            case "syscall":
+
+                break;
+            default:
+                break;
         }
     }
 
